220d55b8c22cbf633117811b1e7da5160b60dd3f
atharv070 <71281747+atharv070@users.noreply.github.com>
2022-09-20 17:53:24 +0530

    Add files via upload

---
+++ if.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Divisor
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = in.nextInt();
+        
+        if (num % 5 == 0 )
+        Int square =num*num
+            System.out.println("Divisible by and 5");
+        
+    }
+}
